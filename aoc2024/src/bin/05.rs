e5d2274aac748b103e302b4f892e5c675b08d5df
Konrad Heidler <k.heidler@tum.de>
2024-12-05 12:00:32 +0100

    2024 Day 5

--- aoc2024/src/bin/05.rs
+++ aoc2024/src/bin/05.rs
@@ -1,14 +1,62 @@
+use std::collections::HashSet;
 use std::error::Error;
 use std::{env, fs::read_to_string};
+
+pub fn parse(input: &str) -> i32 {
+    input.parse().expect("Couldn't parse int")
+}
+
+pub fn parse_rule(input: &str) -> (i32, i32) {
+    let mut nums = input.split('|').map(parse);
+    (nums.next().unwrap(), nums.next().unwrap())
+}
+
+pub fn quicksort<T: IntoIterator<Item = i32>>(vec: T, rules: &HashSet<(i32, i32)>) -> Vec<i32> {
+    let mut numbers = vec.into_iter();
+    match numbers.next() {
+        Some(pivot) => {
+            let (left, right) = numbers.partition::<Vec<i32>, _>(|a| {
+                rules.contains(&(*a, pivot)) && !rules.contains(&(pivot, *a))
+            });
+            return quicksort(left, rules)
+                .into_iter()
+                .chain([pivot].into_iter())
+                .chain(quicksort(right, rules))
+                .collect();
+        }
+        None => {
+            return vec![];
+        }
+    }
+}
 
 pub fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
     let input = read_to_string(&args[1])?;
-    let lines = input.lines().collect::<Vec<_>>();
+    let mut lines = input.lines().peekable();
+    let rules: HashSet<(i32, i32)> = (&mut lines)
+        .take_while(|c| c.contains('|'))
+        .map(parse_rule)
+        .collect();
 
-    println!("Part 1: {}", "todo");
+    let input: Vec<Vec<i32>> = lines
+        .map(|line| line.split(',').map(parse).collect::<Vec<i32>>())
+        .collect();
 
-    println!("Part 2: {}", "todo");
+    let mut part1 = 0;
+    let mut part2 = 0;
+    for line in input.iter() {
+        let sorted = quicksort(line.clone(), &rules);
+        let median = sorted[sorted.len() / 2];
+        if sorted == *line {
+            part1 += median;
+        } else {
+            part2 += median;
+        }
+    }
+    println!("Part 1: {part1}");
+
+    println!("Part 2: {part2}");
 
     Ok(())
 }
