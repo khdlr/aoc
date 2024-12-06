885e7005963e48f24f2bb42bf38e1ec8e896de28
Konrad Heidler <k.heidler@tum.de>
2024-12-06 11:15:14 +0100

    2024 Day 6

--- aoc2024/src/bin/06.rs
+++ aoc2024/src/bin/06.rs
@@ -1,14 +1,148 @@
+use aoc2024::grid::Grid;
+use indicatif::ProgressIterator;
+use std::collections::HashSet;
 use std::error::Error;
+use std::thread::sleep;
+use std::time::Duration;
 use std::{env, fs::read_to_string};
+
+const DEBUG: bool = true;
+
+#[derive(PartialEq, Eq, Hash, Copy, Clone, Debug)]
+enum Dir {
+    N,
+    W,
+    E,
+    S,
+}
+
+#[derive(PartialEq, Eq, Hash, Copy, Clone)]
+struct GuardState {
+    y: i32,
+    x: i32,
+    dir: Dir,
+}
+
+impl GuardState {
+    fn step(&self) -> Self {
+        let mut st = self.clone();
+        match self.dir {
+            Dir::N => st.y -= 1,
+            Dir::W => st.x -= 1,
+            Dir::E => st.x += 1,
+            Dir::S => st.y += 1,
+        };
+        st
+    }
+
+    fn turn_right(&self) -> Self {
+        let mut st = self.clone();
+        match self.dir {
+            Dir::N => st.dir = Dir::E,
+            Dir::W => st.dir = Dir::N,
+            Dir::E => st.dir = Dir::S,
+            Dir::S => st.dir = Dir::W,
+        };
+        st
+    }
+
+    fn pos(&self) -> (i32, i32) {
+        (self.y, self.x)
+    }
+}
+
+fn simulate(grid: &Grid<bool>, state: GuardState) -> (HashSet<GuardState>, bool) {
+    let mut state = state;
+
+    let mut visited = HashSet::from([state]);
+    let mut has_loop = false;
+
+    'outer: loop {
+        let next = state.step();
+        match grid.get((next.y, next.x)) {
+            Some(true) => {
+                state = state.turn_right();
+            }
+            Some(false) => {
+                state = next;
+                if visited.contains(&state) {
+                    has_loop = true;
+                    break 'outer;
+                }
+                visited.insert(state);
+
+                if DEBUG {
+                    let mut g = grid.clone();
+                    g[(state.y, state.x)] = true;
+                    g.print_braille();
+                    sleep(Duration::from_millis(20));
+                }
+            }
+            None => break 'outer,
+        }
+    }
+
+    (visited, has_loop)
+}
 
 pub fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
     let input = read_to_string(&args[1])?;
-    let lines = input.lines().collect::<Vec<_>>();
 
-    println!("Part 1: {}", "todo");
+    let mut init_state = GuardState {
+        y: 0,
+        x: 0,
+        dir: Dir::N,
+    };
+    let grid: Grid<bool> = input
+        .lines()
+        .enumerate()
+        .flat_map(|(iter_y, l)| {
+            l.chars()
+                .enumerate()
+                .map(|(iter_x, c)| {
+                    if c == '^' {
+                        init_state.x = iter_x as i32;
+                        init_state.y = iter_y as i32;
+                    }
+                    return ((iter_y as i32, iter_x as i32), c == '#');
+                })
+                .collect::<Vec<_>>()
+        })
+        .collect();
+    let init_state = init_state; // Remove `mut`
 
-    println!("Part 2: {}", "todo");
+    let disp = grid.map(|b| if *b { '#' } else { '.' });
+    // Part 1
+    let (visited, _has_loop) = simulate(&grid, init_state);
+    let visited_locations: HashSet<(i32, i32)> =
+        visited.iter().map(|state| (state.y, state.x)).collect();
+    println!("Part 1: {:?}", visited_locations.len());
+
+    let mut possible_obstacle_locations: HashSet<(i32, i32)> = HashSet::new();
+    for state in visited {
+        if state.pos() != init_state.pos() {
+            possible_obstacle_locations.insert(state.pos());
+        }
+    }
+
+    println!(
+        "Need to check {} locations for part 2",
+        possible_obstacle_locations.len()
+    );
+
+    let loop_count = possible_obstacle_locations
+        .into_iter()
+        .progress()
+        .filter(|(y, x)| {
+            let mut grid = grid.clone();
+            grid[(*y, *x)] = true;
+            let (_, has_loop) = simulate(&grid, init_state);
+            has_loop
+        })
+        .count();
+
+    println!("Part 2: {loop_count}");
 
     Ok(())
 }
---
+++ aoc2024/src/grid.rs
@@ -0,0 +1,423 @@
+use std::{
+    borrow::Borrow,
+    fmt::Display,
+    ops::{Index, IndexMut},
+    path::Path,
+};
+
+use image::save_buffer;
+
+pub struct GridIterator<'a, T>
+where
+    T: Clone,
+{
+    grid: &'a Grid<T>,
+    pos: (usize, usize),
+}
+
+impl<'a, T> Iterator for GridIterator<'a, T>
+where
+    T: Clone,
+{
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let res = if self.pos.0 < self.grid.height() as usize {
+            Some(&self.grid.grid[self.pos.0][self.pos.1])
+        } else {
+            None
+        };
+
+        let (mut y, mut x) = self.pos;
+        if x < (self.grid.width() - 1) as usize {
+            x += 1;
+        } else {
+            y += 1;
+            x = 0;
+        }
+        self.pos = (y, x);
+        res
+    }
+}
+
+impl<'a, T> GridIterator<'a, T>
+where
+    T: Clone,
+{
+    pub fn with_pos(self) -> GridIteratorWithPos<'a, T> {
+        GridIteratorWithPos::<'a, T> {
+            grid: self.grid,
+            pos: self.pos,
+        }
+    }
+}
+
+pub struct GridIteratorWithPos<'a, T>
+where
+    T: Clone,
+{
+    grid: &'a Grid<T>,
+    pos: (usize, usize),
+}
+
+impl<'a, T> Iterator for GridIteratorWithPos<'a, T>
+where
+    T: Clone,
+{
+    type Item = ((i32, i32), &'a T);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let res = if self.pos.1 < self.grid.height() as usize {
+            Some((
+                (self.pos.0 as i32, self.pos.1 as i32),
+                &self.grid.grid[self.pos.0][self.pos.1],
+            ))
+        } else {
+            None
+        };
+
+        let (mut y, mut x) = self.pos;
+        if x < (self.grid.width() - 1) as usize {
+            x += 1;
+        } else {
+            y += 1;
+            x = 0;
+        }
+        self.pos = (y, x);
+        res
+    }
+}
+
+impl<'a, U, B> FromIterator<((i32, i32), B)> for Grid<U>
+where
+    U: Clone,
+    B: Borrow<U>,
+{
+    fn from_iter<T: IntoIterator<Item = ((i32, i32), B)>>(iter: T) -> Self {
+        let elements: Vec<((i32, i32), U)> = iter
+            .into_iter()
+            .map(|(pos, b)| (pos, b.borrow().clone()))
+            .collect();
+        let h = 1 + elements.iter().map(|el| el.0 .0).max().unwrap() as usize;
+        let w = 1 + elements.iter().map(|el| el.0 .1).max().unwrap() as usize;
+        let grid = vec![vec![elements[0].1.clone(); w]; h];
+        let mut grid = Grid { grid };
+
+        for (pos, val) in elements {
+            grid.set(pos, val.clone());
+        }
+        grid
+    }
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct Grid<T>
+where
+    T: Clone,
+{
+    pub grid: Vec<Vec<T>>,
+}
+
+impl<T> Grid<T>
+where
+    T: Clone,
+{
+    pub fn iter(&self) -> GridIterator<T> {
+        GridIterator::<T> {
+            grid: &self,
+            pos: (0, 0),
+        }
+    }
+
+    pub fn map<U>(&self, mut fun: impl FnMut(&T) -> U) -> Grid<U>
+    where
+        U: Clone,
+    {
+        let grid: Vec<Vec<U>> = self
+            .grid
+            .iter()
+            .map(|row| row.iter().map(|el| fun(el)).collect())
+            .collect();
+        Grid::<U> { grid }
+    }
+
+    pub fn update(&mut self, mut fun: impl FnMut(&T) -> T) {
+        self.grid = self
+            .grid
+            .iter()
+            .map(|row| row.iter().map(|el| fun(el)).collect())
+            .collect();
+    }
+
+    pub fn repeat(&self, vertical: i32, horizontal: i32) -> Grid<T> {
+        let expanded_h: Vec<Vec<T>> = self
+            .grid
+            .iter()
+            .map(|row| (1..=horizontal).flat_map(|_| row.clone()).collect())
+            .collect();
+        let expanded_v: Vec<Vec<T>> = (1..=vertical).flat_map(|_| expanded_h.clone()).collect();
+        Grid::<T> { grid: expanded_v }
+    }
+
+    pub fn map_with_neighbors<U>(&self, mut fun: impl FnMut(&T, Vec<T>) -> U) -> Grid<U>
+    where
+        U: Clone,
+    {
+        let grid = self
+            .grid
+            .iter()
+            .enumerate()
+            .map(|(y, row)| {
+                let y = y as i32;
+                row.iter()
+                    .enumerate()
+                    .map(|(x, el)| -> U {
+                        let x = x as i32;
+                        let neighbors: Vec<T> = [(y - 1, x), (y, x - 1), (y, x + 1), (y + 1, x)]
+                            .iter()
+                            .filter_map(|(y, x)| self.get((*y as i32, *x as i32)))
+                            .collect();
+                        fun(el, neighbors)
+                    })
+                    .collect::<Vec<U>>()
+            })
+            .collect();
+        Grid::<U> { grid }
+    }
+
+    pub fn update_with_neighbors(&mut self, mut fun: impl FnMut(&T, Vec<T>) -> T) {
+        self.grid = self
+            .grid
+            .iter()
+            .enumerate()
+            .map(|(y, row)| {
+                let y = y as i32;
+                row.iter()
+                    .enumerate()
+                    .map(|(x, el)| {
+                        let x = x as i32;
+                        let neighbors: Vec<T> = [(y - 1, x), (y, x - 1), (y, x + 1), (y + 1, x)]
+                            .iter()
+                            .filter_map(|(y, x)| self.get((*y as i32, *x as i32)))
+                            .collect();
+                        fun(el, neighbors)
+                    })
+                    .collect::<Vec<T>>()
+            })
+            .collect();
+    }
+
+    pub fn idxwhere(&self, fun: impl Fn(&T) -> bool) -> Vec<(i32, i32)> {
+        self.grid
+            .iter()
+            .enumerate()
+            .flat_map(|(y, row)| {
+                row.iter()
+                    .enumerate()
+                    .filter_map(|(x, el)| {
+                        if fun(el) {
+                            Some((y as i32, x as i32))
+                        } else {
+                            None
+                        }
+                    })
+                    .collect::<Vec<(i32, i32)>>()
+            })
+            .collect()
+    }
+}
+
+impl<T> Grid<T>
+where
+    T: Clone + Default,
+{
+    pub fn empty(height: i32, width: i32) -> Self {
+        let grid = vec![vec![Default::default(); width as usize]; height as usize];
+        Self { grid }
+    }
+}
+
+impl<T> Index<(i32, i32)> for Grid<T>
+where
+    T: Clone,
+{
+    type Output = T;
+
+    fn index(&self, index: (i32, i32)) -> &Self::Output {
+        &self.grid[index.0 as usize][index.1 as usize]
+    }
+}
+
+impl<T> IndexMut<(i32, i32)> for Grid<T>
+where
+    T: Clone,
+{
+    fn index_mut(&mut self, index: (i32, i32)) -> &mut Self::Output {
+        &mut self.grid[index.0 as usize][index.1 as usize]
+    }
+}
+
+impl<T> Grid<T>
+where
+    T: ToString + Clone,
+{
+    pub fn print(&self) {
+        for row in &self.grid {
+            let line: String = row.iter().map(|c| c.to_string()).collect::<String>();
+            println!("{}", line);
+        }
+    }
+}
+
+impl Grid<char> {
+    pub fn from_str(str: &str) -> Self {
+        let grid = str.lines().map(|line| line.chars().collect()).collect();
+        Self { grid }
+    }
+}
+
+impl Grid<usize> {
+    pub fn parse(str: &str) -> Self {
+        let grid = str
+            .lines()
+            .map(|line| {
+                line.chars()
+                    .map(|c| c.to_string().parse::<usize>().unwrap())
+                    .collect()
+            })
+            .collect();
+        Self { grid }
+    }
+}
+
+impl<T> Grid<T>
+where
+    T: Clone + Display,
+{
+    pub fn print_overlay(&self, fun: impl Fn((i32, i32)) -> Option<char>) {
+        for i in 0..self.height() {
+            for j in 0..self.width() {
+                match fun((i, j)) {
+                    Some(chr) => print!("{}", chr),
+                    None => print!("{}", self.get((i, j)).unwrap()),
+                }
+            }
+            println!();
+        }
+    }
+}
+
+impl<T> Grid<T>
+where
+    T: Clone,
+{
+    pub fn get(&self, pos: (i32, i32)) -> Option<T> {
+        let (y, x) = pos;
+        if y < 0 || x < 0 {
+            None
+        } else {
+            self.grid.get(y as usize).cloned()?.get(x as usize).cloned()
+        }
+    }
+
+    pub fn get_wrap(&self, pos: (i32, i32)) -> T {
+        let (mut y, mut x) = pos;
+        let h = self.height();
+        y = ((y % h) + h) % h;
+        let w = self.height();
+        x = ((x % w) + w) % w;
+        self.grid[y as usize][x as usize].clone()
+    }
+
+    pub fn set(&mut self, pos: (i32, i32), val: T) {
+        assert!(pos.0 >= 0, "y < 0");
+        assert!(pos.1 >= 0, "x < 0");
+        self.grid[pos.0 as usize][pos.1 as usize] = val;
+    }
+
+    pub fn height(&self) -> i32 {
+        self.grid.len() as i32
+    }
+
+    pub fn width(&self) -> i32 {
+        self.grid[0].len() as i32
+    }
+
+    pub fn print_map<F, S>(&self, fun: F)
+    where
+        F: Fn(&T) -> S,
+        S: Into<String>,
+    {
+        for row in &self.grid {
+            let line: String = row.iter().map(|c| fun(&c).into()).collect::<String>();
+            println!("{}", line);
+        }
+    }
+
+    pub fn save_img(&self, filename: &str, fun: impl Fn(&T) -> (u8, u8, u8)) {
+        let mut bytes: Vec<u8> =
+            Vec::with_capacity(self.height() as usize * self.width() as usize * 3);
+        for row in &self.grid {
+            for val in row {
+                let (r, g, b) = fun(val);
+                bytes.push(r);
+                bytes.push(g);
+                bytes.push(b);
+            }
+        }
+        // let img Image
+        let _ = save_buffer(
+            &Path::new(filename),
+            &bytes,
+            self.width() as u32,
+            self.height() as u32,
+            image::ColorType::Rgb8,
+        );
+    }
+}
+
+impl<T> Display for Grid<T>
+where
+    T: Display + Clone,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for i in 0..self.height() {
+            for j in 0..self.width() {
+                write!(f, "{}", self.get((i, j)).unwrap())?;
+            }
+            writeln!(f)?;
+        }
+        Ok(())
+    }
+}
+
+impl Grid<bool> {
+    pub fn print_braille(&self) {
+        let mut chars = String::new();
+        for y0 in (0..(self.height() + 3)).step_by(4) {
+            for x0 in (0..(self.width() + 1)).step_by(2) {
+                // Determine character code
+                let mut c: u32 = 0x2800;
+                let mut pow = 1;
+                for (dy, dx) in [
+                    (0, 0),
+                    (1, 0),
+                    (2, 0),
+                    (0, 1),
+                    (1, 1),
+                    (2, 1),
+                    (3, 0),
+                    (3, 1),
+                ] {
+                    if let Some(true) = self.get((y0 + dy, x0 + dx)) {
+                        c += pow;
+                    }
+                    pow *= 2
+                }
+                chars.push(char::from_u32(c).expect("Broken implementation for braille"));
+            }
+            chars.push('\n');
+        }
+        println!("{chars}");
+    }
+}
