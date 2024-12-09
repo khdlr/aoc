d942c6349789e08d92373c27b11de5cfcff8d6cf
Konrad Heidler <k.heidler@tum.de>
2024-12-09 21:58:48 +0100

    2015 Day 3

--- aoc2015/src/bin/03.rs
+++ aoc2015/src/bin/03.rs
@@ -1,14 +1,54 @@
+use std::collections::HashSet;
 use std::error::Error;
 use std::{env, fs::read_to_string};
 
 pub fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
     let input = read_to_string(&args[1])?;
-    let lines = input.lines().collect::<Vec<_>>();
+    let line = input.lines().next().expect("No lines in input");
+    let mut y = 0;
+    let mut x = 0;
+    let mut visited: HashSet<(i32, i32)> = HashSet::from([(0, 0)]);
 
-    println!("Part 1: {}", "todo");
+    for mov in line.chars() {
+        let (dy, dx) = match mov {
+            'v' => (1, 0),
+            '^' => (-1, 0),
+            '>' => (0, 1),
+            '<' => (0, -1),
+            c => panic!("Unknown char: `{c}`"),
+        };
+        y += dy;
+        x += dx;
+        visited.insert((y, x));
+    }
 
-    println!("Part 2: {}", "todo");
+    println!("Part 1: {}", visited.len());
+
+    let mut r_y = 0;
+    let mut r_x = 0;
+    let mut s_y = 0;
+    let mut s_x = 0;
+    let mut visited: HashSet<(i32, i32)> = HashSet::from([(0, 0)]);
+    for (i, mov) in line.chars().enumerate() {
+        let (dy, dx) = match mov {
+            'v' => (1, 0),
+            '^' => (-1, 0),
+            '>' => (0, 1),
+            '<' => (0, -1),
+            c => panic!("Unknown char: `{c}`"),
+        };
+        if i % 2 == 0 {
+            s_y += dy;
+            s_x += dx;
+            visited.insert((s_y, s_x));
+        } else {
+            r_y += dy;
+            r_x += dx;
+            visited.insert((r_y, r_x));
+        }
+    }
+    println!("Part 2: {}", visited.len());
 
     Ok(())
 }
