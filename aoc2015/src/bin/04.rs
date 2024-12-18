c29a8160c635b53a6a407a91e3305d2b557b127f
Konrad Heidler <k.heidler@tum.de>
2024-12-18 15:28:13 +0100

    2015 Day 4

--- aoc2015/src/bin/04.rs
+++ aoc2015/src/bin/04.rs
@@ -4,11 +4,24 @@ use std::{env, fs::read_to_string};
 pub fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
     let input = read_to_string(&args[1])?;
-    let lines = input.lines().collect::<Vec<_>>();
+    let key = input.lines().next().expect("No input");
+    for num in 1.. {
+        let block = format!("{key}{num}");
+        let hash = format!("{:x}", md5::compute(block));
+        if hash.starts_with("00000") {
+            println!("Part1: {num}");
+            break;
+        }
+    }
 
-    println!("Part 1: {}", "todo");
-
-    println!("Part 2: {}", "todo");
+    for num in 1.. {
+        let block = format!("{key}{num}");
+        let hash = format!("{:x}", md5::compute(block));
+        if hash.starts_with("000000") {
+            println!("Part1: {num}");
+            break;
+        }
+    }
 
     Ok(())
 }
