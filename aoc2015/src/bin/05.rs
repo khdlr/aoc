ff386691d7c37afcd8adef54080e4735abba31ed
Konrad Heidler <k.heidler@tum.de>
2024-12-18 15:42:40 +0100

    2015 Day 5

--- aoc2015/src/bin/05.rs
+++ aoc2015/src/bin/05.rs
@@ -1,14 +1,43 @@
 use std::error::Error;
 use std::{env, fs::read_to_string};
 
+fn is_nice(inp: &&&str) -> bool {
+    let vowel_count = inp
+        .chars()
+        .filter(|&c| matches!(c, 'a' | 'e' | 'i' | 'o' | 'u'))
+        .count();
+    let has_repeat = inp.chars().zip(inp.chars().skip(1)).any(|(a, b)| a == b);
+    let has_forbidden = ["ab", "cd", "pq", "xy"]
+        .into_iter()
+        .any(|seq| inp.contains(seq));
+
+    return (vowel_count >= 3) && has_repeat && !has_forbidden;
+}
+
+fn is_nice_v2(inp: &&&str) -> bool {
+    let mut xyxy = false;
+    let mut xyx = false;
+    let chrs: Vec<_> = inp.chars().collect();
+    for i in 0..inp.len() - 2 {
+        xyx |= chrs[i] == chrs[i + 2];
+        for j in i + 2..inp.len() - 1 {
+            xyxy |= (chrs[i] == chrs[j]) && (chrs[i + 1] == chrs[j + 1])
+        }
+    }
+
+    xyxy && xyx
+}
+
 pub fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
     let input = read_to_string(&args[1])?;
-    let lines = input.lines().collect::<Vec<_>>();
+    let lines: Vec<&str> = input.lines().collect::<Vec<_>>();
 
-    println!("Part 1: {}", "todo");
+    let part1 = lines.iter().filter(is_nice).count();
+    println!("Part 1: {}", part1);
 
-    println!("Part 2: {}", "todo");
+    let part2 = lines.iter().filter(is_nice_v2).count();
+    println!("Part 2: {}", part2);
 
     Ok(())
 }
