2fbda9ad8a797dfd3a8e7bc24dc740797dbe9db1
Konrad Heidler <k.heidler@tum.de>
2024-12-19 10:46:04 +0100

    2024 Day 19 Part 1

--- aoc2024/src/bin/19.rs
+++ aoc2024/src/bin/19.rs
@@ -1,12 +1,46 @@
 use std::error::Error;
 use std::{env, fs::read_to_string};
 
+pub fn dfs(combination: &Vec<char>, towels: &Vec<Vec<char>>, matched: usize) -> Option<()> {
+    if matched == combination.len() {
+        return Some(());
+    } else if matched > combination.len() {
+        return None;
+    }
+    for towel in towels.iter() {
+        if towel
+            .iter()
+            .zip(combination.iter().skip(matched))
+            .all(|(a, b)| a == b)
+        {
+            // Towel matches!
+            if let Some(_) = dfs(combination, towels, matched + towel.len()) {
+                return Some(());
+            }
+        }
+    }
+    None
+}
+
 pub fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
     let input = read_to_string(&args[1])?;
-    let lines = input.lines().collect::<Vec<_>>();
+    let mut lines = input.lines();
+    let towels: Vec<Vec<char>> = lines
+        .next()
+        .expect("No towels")
+        .split(", ")
+        .map(|s| s.chars().collect())
+        .collect();
+    assert!(lines.next().expect("No second line") == "", "No empty line");
+    let combinations = lines.map(|c| c.chars().collect()).collect::<Vec<_>>();
 
-    println!("Part 1: {}", "todo");
+    let part1 = combinations
+        .iter()
+        .filter_map(|c| dfs(c, &towels, 0))
+        .count();
+
+    println!("Part 1: {}", part1);
 
     println!("Part 2: {}", "todo");
 
